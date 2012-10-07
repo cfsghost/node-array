c5891781bef13ac6d5164dab1ad4cf6ab60878b0
Fred Chien <fred@mandice.com>
2012-10-08 03:56:51 +0800

    little fixes

--- tests/async_array.js
+++ tests/async_array.js
@@ -2,8 +2,10 @@ var Array = require('../');
 
 var a = [ 1, 2, 3, 4, 5 ];
 
-a.forEachAsync(function(element, index, complete) {
+a.forEachAsync(function(element, index, arr) {
 	console.log(element);
+	console.log(index);
+	console.log(arr);
 }, function() {
 	console.log('complete');
 });
--- tests/async_array_break.js
+++ tests/async_array_break.js
@@ -2,7 +2,7 @@ var Array = require('../');
 
 var a = [ 1, 2, 3, 4, 5 ];
 
-a.forEachAsync(function(element, index, complete) {
+a.forEachAsync(function(element, index, arr) {
 
 	if (index == 2)
 		return false;
